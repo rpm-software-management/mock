226ffeaa2cc8d41157426a37b35b8832a1fd0d3c
skvidal <skvidal>
2005-05-16 06:27:02 +0000

    - update copyright info - update README with some more content and TODOs

--- mock.py
+++ mock.py
@@ -12,6 +12,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+# Written by Seth Vidal
+# Sections taken from Mach by Thomas Vander Stichele
 
 import os
 import os.path
