5c2aba6ac0472e6ee2e7c4c4bc7f58c515518b22
Michael E Brown <michael_e_brown@dell.com>
2007-10-16 16:20:18 -0500

    add vim declarations.

--- src/py-libs/exception.py
+++ src/py-libs/exception.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python -tt
+# vim:expandtab:autoindent:tabstop=4:shiftwidth=4:filetype=python:textwidth=0:
+#
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
