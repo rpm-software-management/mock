5215493dd570f711a4594eab8cf34a8cb7718a66
Michael E Brown <michael_e_brown@dell.com>
2007-10-17 16:17:42 -0500

    finish out setup of chroot env.

--- src/py-libs/backend.py
+++ src/py-libs/backend.py
@@ -22,9 +22,11 @@
 import logging
 import os
 import shutil
+import stat
 
 # our imports
 import mock.util
+import mock.exception
 from mock.trace_decorator import traceLog
 
 # set up logging
@@ -68,9 +70,14 @@ class Root(object):
 
         # config options
         self.chrootuid = config['chrootuid']
+        self.chrootuser = config['chrootuser']
+        self.chrootgroup = config['chrootgroup']
         self.yum_conf_content = config['yum.conf']
         self.use_host_resolv = config['use_host_resolv']
         self.chroot_file_contents = config['files']
+        self.chroot_setup_cmd = config['chroot_setup_cmd']
+        self.yum_path = '/usr/bin/yum'
+        self.macros = config['macros']
 
         # officially set state so it is logged
         self.state("unstarted")
@@ -92,11 +99,7 @@ class Root(object):
         """clean out chroot with extreme prejudice :)"""
         self.state("clean")
         self.root_log.info("Cleaning chroot")
-        try:
-            shutil.rmtree(self.basedir)
-        except OSError, e:
-            if e.errno != 2: # no such file or directory
-                raise
+        mock.util.rmtree(self.basedir)
 
     @traceLog(moduleLog)
     def init(self):
@@ -129,9 +132,9 @@ class Root(object):
         self.root_log.info('create skeleton dirs')
         for item in [
                      os.path.join(self.rootdir, 'var/lib/rpm'),
+                     os.path.join(self.rootdir, 'var/lib/yum'),
                      os.path.join(self.rootdir, 'var/log'),
                      os.path.join(self.rootdir, 'var/lock/rpm'),
-                     os.path.join(self.rootdir, 'dev'),
                      os.path.join(self.rootdir, 'etc/rpm'),
                      os.path.join(self.rootdir, 'tmp'),
                      os.path.join(self.rootdir, 'var/tmp'),
@@ -150,7 +153,7 @@ class Root(object):
         # write in yum.conf into chroot
         # always truncate and overwrite (w+)
         self.root_log.info('configure yum')
-        yumconf = os.path.join(self.rootdir, 'etc', 'yum.conf')
+        yumconf = os.path.join(self.rootdir, 'etc', 'yum', 'yum.conf')
         yumconf_fo = open(yumconf, 'w+')
         yumconf_fo.write(self.yum_conf_content)
         yumconf_fo.close()
@@ -172,17 +175,125 @@ class Root(object):
                 fo.write(self.chroot_file_contents[key])
                 fo.close()
 
+        # files in /dev
+        mock.util.rmtree(os.path.join(self.rootdir, "dev"))
+        mock.util.mkdirIfAbsent(os.path.join(self.rootdir, "dev", "pts"))
+        os.mknod(os.path.join(self.rootdir, "dev/zero"), stat.S_IFCHR | 0666, os.makedev(1, 5))
+        os.mknod(os.path.join(self.rootdir, "dev/null"), stat.S_IFCHR | 0666, os.makedev(1, 3))
+        os.mknod(os.path.join(self.rootdir, "dev/random"), stat.S_IFCHR | 0666, os.makedev(1, 8))
+        os.mknod(os.path.join(self.rootdir, "dev/urandom"), stat.S_IFCHR | 0444, os.makedev(1, 9))
+        os.mknod(os.path.join(self.rootdir, "dev/console"), stat.S_IFCHR | 0600, os.makedev(5, 1))
+        os.symlink("/proc/self/fd/0", os.path.join(self.rootdir, "dev/stdin"))
+        os.symlink("/proc/self/fd/1", os.path.join(self.rootdir, "dev/stdout"))
+        os.symlink("/proc/self/fd/2", os.path.join(self.rootdir, "dev/stderr"))
+        # "/dev/log" 
+
         # yum stuff
+        self.root_log.info('run yum')
+        self._mountall()
+        self._yum(self.chroot_setup_cmd)
+        self._umountall()
+
         # create user
+        self._make_our_user()
+
         # create rpmbuild dir
-   
+        self._build_dir_setup()
+
+    @traceLog(moduleLog)
+    def close(self):
+        """cleanup everything"""
+        self.state("ending")
+        self.state("done")
+        self.root_log.info("Done")
 
     # =============
     # 'Private' API
     # =============
     @traceLog(moduleLog)
-    def _yum(self):
-        pass
+    def _mountall(self):
+        """mount 'normal' fs like /dev/ /proc/ /sys"""
+        cmds = ('mount -t proc   mock_chroot_proc   %s/proc' % self.rootdir,
+                'mount -t devpts mock_chroot_devpts %s/dev/pts' % self.rootdir,
+                'mount -t sysfs  mock_chroot_sysfs  %s/sys' % self.rootdir,
+               )
+    
+        g = [s%self.rootdir for s in ("%s/proc", "%s/dev/pts", "%s/sys")]
+        mock.util.mkdirIfAbsent(*g)
+        for cmd in cmds:
+            self.root_log.info(cmd)
+            mock.util.do(cmd)
+
+    @traceLog(moduleLog)
+    def _umountall(self):
+        """umount all mounted chroot fs."""
+        cmds = ('umount %s/proc' % self.rootdir,
+                'umount %s/dev/pts' % self.rootdir,
+                'umount %s/sys' % self.rootdir,
+               )
+    
+        for cmd in cmds:
+            self.root_log.info(cmd)
+            mock.util.do(cmd)
+
+    @traceLog(moduleLog)
+    def _yum(self, cmd):
+        """use yum to install packages/package groups into the chroot"""
+        # mock-helper yum --installroot=rootdir cmd
+        cmd = '%s --installroot %s %s' % (self.yum_path, self.rootdir, cmd)
+        self.root_log.info(cmd)
+        try:
+            mock.util.do(cmd)
+        except mock.exception.Error, e:
+            self.root_log.exception("Error performing yum command: %s" % cmd)
+            raise mock.exception.YumError, "Error performing yum command: %s" % cmd
+
+    @traceLog(moduleLog)
+    def _do_chroot(self, command, *args, **kargs):
+        """execute given command in root"""
+        cmd = "/usr/sbin/chroot %s %s" % (self.rootdir, command)
+        return mock.util.do(cmd, *args, **kargs)
+
+    @traceLog(moduleLog)
+    def _make_our_user(self):
+        if not os.path.exists(os.path.join(self.rootdir, 'usr/sbin/useradd')):
+            raise RootError, "Could not find useradd in chroot, maybe the install failed?"
+
+        mock.util.rmtree(os.path.join(self.rootdir, self.homedir))
+        self._do_chroot('/usr/sbin/userdel -r %s' % self.chrootuser, raiseExc=False)
+        self._do_chroot('/usr/sbin/groupdel %s' % self.chrootgroup, raiseExc=False)
+        self._do_chroot('/usr/sbin/useradd -m -u %s -d %s %s' % (self.chrootuid, self.homedir, self.chrootuser), raiseExc=True)
+
+
+    #
+    # UNPRIVLEGED: 
+    #   Everything in this function runs as the build user
+    #
+    @traceLog(moduleLog)
+    def _build_dir_setup(self):
+        # create all dirs as the user who will be dropping things there.
+        self.uidManager.becomeUser(self.chrootuid)
+        try:
+            # create dir structure
+            for subdir in ["%s/%s/%s" % (self.rootdir, self.homedir, s) for s in ('RPMS', 'SRPMS', 'SOURCES', 'SPECS', 'BUILD', 'originals')]:
+                mock.util.mkdirIfAbsent(subdir)
+
+            # change ownership so we can write to build home dir
+            for (dirpath, dirnames, filenames) in os.walk(self.homedir):
+                for path in dirnames + filenames:
+                    os.chown(os.path.join(dirpath, path), self.chrootuid, -1)
+                    os.chmod(os.path.join(dirpath, path), 0755)
+            
+            # rpmmacros default
+            self.macros['%_rpmlock_path'] = "%s/var/lib/rpm/__db.000" % self.basedir
+            macrofile_out = '%s%s/.rpmmacros' % (self.rootdir, self.homedir)
+            rpmmacros = open(macrofile_out, 'w+')
+            for key, value in self.macros.items():
+                rpmmacros.write( "%s %s\n" % (key, value) )
+            rpmmacros.close()
+
+        finally:
+            self.uidManager.elevatePrivs()
     
     @traceLog(moduleLog)
     def _resetLogging(self):
