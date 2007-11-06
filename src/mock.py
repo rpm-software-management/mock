01335086781d49c37c81d08ff19c66c89c21e0ec
Michael E Brown <mebrown@michaels-house.net>
2007-11-06 17:29:58 -0600

    version bump

--- src/mock.py
+++ src/mock.py
@@ -30,7 +30,7 @@ import time
 from optparse import OptionParser
 
 # all of the variables below are substituted by the build system
-__VERSION__="0.8.5"
+__VERSION__="0.8.7"
 SYSCONFDIR="/usr/local/etc"
 PYTHONDIR="/usr/local/lib/python2.5/site-packages"
 PKGPYTHONDIR="/usr/local/lib/python2.5/site-packages/mock"
