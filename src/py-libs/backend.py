21678b7e7720724f36f852e7ddf3cf34bd5ed349
Michael E Brown <mebrown@michaels-house.net>
2007-11-15 12:13:03 -0600

    'expect' wont work without /dev/ptmx.

--- src/py-libs/backend.py
+++ src/py-libs/backend.py
@@ -240,6 +240,7 @@ class Root(object):
         os.mknod(os.path.join(self.rootdir, "dev/tty"), stat.S_IFCHR | 0666, os.makedev(5, 0))
         os.mknod(os.path.join(self.rootdir, "dev/null"), stat.S_IFCHR | 0666, os.makedev(1, 3))
         os.mknod(os.path.join(self.rootdir, "dev/random"), stat.S_IFCHR | 0666, os.makedev(1, 8))
+        os.mknod(os.path.join(self.rootdir, "dev/ptmx"), stat.S_IFCHR | 0666, os.makedev(5, 2))
         os.mknod(os.path.join(self.rootdir, "dev/urandom"), stat.S_IFCHR | 0444, os.makedev(1, 9))
         os.mknod(os.path.join(self.rootdir, "dev/console"), stat.S_IFCHR | 0600, os.makedev(5, 1))
         os.symlink("/proc/self/fd/0", os.path.join(self.rootdir, "dev/stdin"))
