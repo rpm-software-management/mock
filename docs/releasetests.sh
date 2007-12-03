1e4847aab547ca88227315133fc4b1996209e53c
Michael E Brown <mebrown@michaels-house.net>
2007-12-03 14:26:43 -0600

    add couple of comments to release test script.

--- docs/releasetests.sh
+++ docs/releasetests.sh
@@ -5,10 +5,15 @@
 # release. The basic idea is:
 
 # 1) make distcheck to ensure that all autoconf stuff is setup properly
-# 2) rebuild mock srpm using this version of mock under all distributed configs
+# 2) run some basic tests to test different mock options.
+# 3) rebuild mock srpm using this version of mock under all distributed configs
 
 # This test will only run on a machine with full access to internet.
 # might work with http_proxy= env var, but I havent tested that.
+# 
+# This test script expects to be run on an x86_64 machine. It will *not* run
+# properly on an i386 machine.
+#
 
 set -e
 set -x
