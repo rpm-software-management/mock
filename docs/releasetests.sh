d91271d37a6155fd45f82f2d4c7289acbef0674b
Michael E Brown <michael_e_brown@dell.com>
2007-12-10 14:15:48 -0600

    fixup explicit orphankill unit test.

--- docs/releasetests.sh
+++ docs/releasetests.sh
@@ -82,8 +82,16 @@ fi
 time $MOCKCMD --offline --init
 cp docs/daemontest $CHROOT/tmp
 echo -e "#!/bin/sh\n/tmp/daemontest\nsleep 60\n" >> $CHROOT/tmp/try
-$MOCKCMD --offline --chroot -- /tmp/try &
-kill -9 $!
+# the following should launch about three processes in the chroot: bash, sleep, daemontest
+$MOCKCMD --offline --chroot -- bash /tmp/try &
+mockpid=$!
+sleep 1
+# now we 'prematurely' kill mock. This should leave the three orphans above
+sudo kill -9 $mockpid
+if ! pgrep daemontest; then
+    echo "Daemontest failed. daemontest should be running now but is not."
+    exit 1
+fi
 $MOCKCMD --offline --orphanskill 
 if pgrep daemontest; then
     echo "Daemontest FAILED. found a daemontest process running after exit." 
