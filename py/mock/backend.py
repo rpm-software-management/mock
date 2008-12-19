78cd2d318fb16777db57af335c2e82db3c06c3f7
Clark Williams <williams@redhat.com>
2008-12-19 15:58:06 -0600

    added version variable; added Mock Version banner to all logs

--- py/mock/backend.py
+++ py/mock/backend.py
@@ -31,6 +31,7 @@ class Root(object):
         self.preExistingDeps = ""
         self.logging_initialized = False
         self.buildrootLock = None
+        self.version = config['version']
 
         self.sharedRootName = config['root']
         if config.has_key('unique-ext'):
@@ -544,6 +545,7 @@ class Root(object):
                 fh.setFormatter(formatter)
                 fh.setLevel(logging.NOTSET)
                 log.addHandler(fh)
+                log.info("Mock Version: %s" % self.version)
         finally:
             self.uidManager.restorePrivs()
 
