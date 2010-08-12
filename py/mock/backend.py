rpm-software-management/mock

commit 3bc5fb958deb809fc04d6a74ce6688e093f5831a

Don't automatically update the chroot in a --no-clean scenario
This causes largely unnecessary yum runs and delays builds.
The new behavior would just skip the yum call all together, unless
the chroot was cleaned, in which case the setup command is called.

py/mock/backend.py
@@ -269,9 +269,8 @@ class Root(object):
         self.state("running yum")
         try:
             self._mountall()
-            if not self.chrootWasCleaned:
-                self.chroot_setup_cmd = 'update'
-            self._yum(self.chroot_setup_cmd, returnOutput=1)
+            if self.chrootWasCleaned:
+                self._yum(self.chroot_setup_cmd, returnOutput=1)
         finally:
             self._umountall()
 

commit df1f05e2e05b124422f1427f93b6dec580812809

Update packages after unpacking root cache
Fix problem introduced in commit 3bc5fb958deb809fc04d6a74ce6688e093f5831a
where the buildroot is no longer updated after unpacking the root cache.

http://bugzilla.redhat.com/557526

Signed-off-by: Clark Williams <williams@redhat.com>

py/mock/backend.py
@@ -34,6 +34,7 @@ class Root(object):
         self._state = 'unstarted'
         self.uidManager = uidManager
         self._hooks = {}
+        self.chrootWasCached = False
         self.chrootWasCleaned = False
         self.preExistingDeps = ""
         self.logging_initialized = False
@@ -311,6 +312,8 @@ class Root(object):
             self._mountall()
             if self.chrootWasCleaned:
                 self._yum(self.chroot_setup_cmd, returnOutput=1)
+            if self.chrootWasCached:
+                self._yum('update', returnOutput=1)
 
             # create user
             self._makeBuildUser()

py/mock/plugins/root_cache.py
@@ -86,7 +86,7 @@ class RootCache(object):
         except OSError:
             pass
 
-        # optimization: dont unpack root cache if chroot was not cleaned
+        # optimization: don't unpack root cache if chroot was not cleaned
         if os.path.exists(self.rootCacheFile) and self.rootObj.chrootWasCleaned:
             self.state("unpacking root cache")
             self._rootCacheLock()
@@ -95,8 +95,8 @@ class RootCache(object):
                 shell=False
                 )
             self._rootCacheUnlock()
-            self.chroot_setup_cmd = "update"
             self.rootObj.chrootWasCleaned = False
+            self.rootObj.chrootWasCached = True
 
     decorate(traceLog())
     def _rootCachePostInitHook(self):