9afe90fec2118689e20af652d3756c93b9d06bdc
Clark Williams <williams@redhat.com>
2011-02-18 15:16:21 -0600

    commented out /dev/tty handling code in backend.py
    Prevent creation of /dev/tty (either special file or symlink to
    /dev/ptmx). This is so interactive test suites being built will not
    hang when attempting to talk to a controlling terminal
    
    Signed-off-by: Clark Williams <williams@redhat.com>

--- py/mock/backend.py
+++ py/mock/backend.py
@@ -411,22 +411,23 @@ class Root(object):
         os.symlink("/proc/self/fd/2", self.makeChrootPath("dev/stderr"))
 
         # if hosted on EL{4,5} create /dev/tty node in chroot
-        if mock.util.cmpKernelEVR(kver, '2.6.19') <= 0:
-            os.mknod(self.makeChrootPath("dev/tty", stat.S_IFCHR|0666, os.makedev(5, 0)))
-            getLog().debug("created /dev/tty node device node")
+        #if mock.util.cmpKernelEVR(kver, '2.6.19') <= 0:
+        #    os.mknod(self.makeChrootPath("dev/tty", stat.S_IFCHR|0666, os.makedev(5, 0)))
+        #    getLog().debug("created /dev/tty node device node")
 
         # symlink /dev/fd in the chroot for everything except RHEL4
         if mock.util.cmpKernelEVR(kver, '2.6.9') > 0:
             os.symlink("/proc/self/fd",   self.makeChrootPath("dev/fd"))
 
+        # comment the below out for now
         # symlinks for Fedora and el6 hosts
-        if mock.util.cmpKernelEVR(kver, '2.6.19') > 0:
-            os.symlink("/dev/pts/ptmx", self.makeChrootPath("dev/ptmx"))
-            if os.path.exists(self.makeChrootPath("dev/tty")):
-                getLog().debug("removed dev/tty device node!")
-                os.remove(self.makeChrootPath("dev/tty"))
-            os.symlink("/dev/ptmx", self.makeChrootPath("dev/tty"))
-            getLog().debug("symlinked dev/tty to ptmx")
+        # if mock.util.cmpKernelEVR(kver, '2.6.19') > 0:
+        #    os.symlink("/dev/pts/ptmx", self.makeChrootPath("dev/ptmx"))
+        #    if os.path.exists(self.makeChrootPath("dev/tty")):
+        #        getLog().debug("removed dev/tty device node!")
+        #        os.remove(self.makeChrootPath("dev/tty"))
+        #    os.symlink("/dev/ptmx", self.makeChrootPath("dev/tty"))
+        #    getLog().debug("symlinked dev/tty to ptmx")
 
         os.umask(prevMask)
 
