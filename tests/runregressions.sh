b43a280f081bd9431d1608eb55df7699e95c0776
Clark Williams <williams@redhat.com>
2012-05-10 13:48:55 -0500

    added new files to test suite
    Signed-off-by: Clark Williams <williams@redhat.com>

---
+++ tests/runregressions.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+
+if [ "$MOCKCMD" = "" ]
+then
+    d=$(cd $(dirname $0); pwd)
+    source $d/testenvironment
+fi
+
+source ${TESTDIR}/functions
+
+cd $TOPDIR
+
+trap '$MOCKCMD --clean; exit 1' INT HUP QUIT TERM
+
+$MOCKCMD --init
+
+fails=0
+#
+# run regression tests
+#
+header "running regression tests"
+for i in ${TESTDIR}/*.tst; do
+    sh $i
+    if [ $? != 0 ]; then
+	fails=$(($fails + 1))
+	echo "*  FAILED: $i"
+    else
+	echo "*  PASSED: $i"
+    fi
+    echo "****************************************************"
+done
+
+msg=$(printf "%d regression failures\n" $fails)
+header "$msg"
+
+#
+# clean up
+#
+header "clean up from first round of tests"
+runcmd "$MOCKCMD --offline --clean"
+
+exit $fails
