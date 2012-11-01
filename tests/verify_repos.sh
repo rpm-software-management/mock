853cf690ddada41f5b2f4dcc8a085b8093f6cda7
Clark Williams <williams@redhat.com>
2012-11-01 13:22:27 -0500

    inital checkin of script to verify cfg repos
    Signed-off-by: Clark Williams <williams@redhat.com>

---
+++ tests/verify_repos.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+cfgs=$(grep 'mirrorlist' ../etc/mock/*.cfg | sed -e 's/^.*mirrorlist=//')
+tmpfile=$(mktemp)
+for c in $cfgs; do
+    wget -q -O $tmpfile $c
+    lines=$(wc -l $tmpfile | awk '{print $1}')
+    if [ $lines == 1 ]; then
+	echo "!!! $c is not a valid URL!!!"
+    fi
+done
