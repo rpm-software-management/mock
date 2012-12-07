50b83f20fc55ac39b630702dc9ca8b9356e4b6f6
Clark Williams <williams@redhat.com>
2012-12-07 13:47:27 -0600

    fix initialization of package_state plugin
    Added package_state to the configured plugins array so that it is
    enabled by default.
    
    Signed-off-by: Clark Williams <williams@redhat.com>

--- py/mock.py
+++ py/mock.py
@@ -278,7 +278,8 @@ def setup_default_config_opts(config_opts, unprivUid):
     #    root_cache next.
     #    after that, any plugins that must create dirs (yum_cache)
     #    any plugins without preinit hooks should be last.
-    config_opts['plugins'] = ['tmpfs', 'root_cache', 'yum_cache', 'bind_mount', 'ccache', 'selinux']
+    config_opts['plugins'] = ['tmpfs', 'root_cache', 'yum_cache', 'bind_mount', 'ccache', 'selinux', 
+                              'package_state']
     config_opts['plugin_dir'] = os.path.join(PKGPYTHONDIR, "plugins")
     config_opts['plugin_conf'] = {
             'ccache_enable': True,
