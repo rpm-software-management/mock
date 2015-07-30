466f066f67fa0b02429e64f88705ed377414751f
Miroslav Suchý <msuchy@redhat.com>
2015-07-30 11:29:03 +0200

    better parsing of content-disposition header [RHBZ#1248344]

--- py/mockchain.py
+++ py/mockchain.py
@@ -26,15 +26,16 @@ try:
     from six.moves.urllib_parse import urlsplit
 except ImportError:
     from urlparse import urlsplit
-import sys
-import subprocess
-import os
+import cgi
 import optparse
-import requests
-import tempfile
-import shutil
-import time
+import os
 import re
+import requests
+import shutil
+import subprocess
+import sys
+import tempfile
+import time
 
 import mockbuild.util
 
@@ -325,9 +326,9 @@ def main(args):
                     if r.status_code == requests.codes.ok:
                         fn = urlsplit(r.url).path.rsplit('/', 1)[1]
                         if 'content-disposition' in r.headers:
-                            header_cd = r.headers['content-disposition'].split('filename=')
-                            if len(header_cd) > 1:
-                                fn = header_cd[1]
+                            _, params = cgi.parse_header(r.headers['content-disposition'])
+                            if 'filename' in params and params['filename']:
+                                fn = params['filename']
                         pkg = download_dir + '/' + fn
                         fd = open(pkg, 'wb')
                         for chunk in r.iter_content(4096):
