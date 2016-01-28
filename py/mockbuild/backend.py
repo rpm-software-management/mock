13ac0e8829b0404e919552c3d2e5bca8d93f3e2c
Miroslav Suchý <msuchy@redhat.com>
2016-01-28 15:00:01 +0100

    C:405, 0: Wrong continued indentation.

--- py/mockbuild/backend.py
+++ py/mockbuild/backend.py
@@ -395,11 +395,11 @@ class Commands(object):
             command=self.config['rpmbuild_command'])]
         if not util.USE_NSPAWN:
             command = ["bash", "--login", "-c"] + command
-        self.buildroot.doChroot(
-            command, shell=False, logger=self.buildroot.build_log,
-            timeout=timeout, uid=self.buildroot.chrootuid,
-            gid=self.buildroot.chrootgid, user=self.buildroot.chrootuser,
-            printOutput=self.config['print_main_output'])
+        self.buildroot.doChroot(command,
+                                shell=False, logger=self.buildroot.build_log, timeout=timeout,
+                                uid=self.buildroot.chrootuid, gid=self.buildroot.chrootgid,
+                                user=self.buildroot.chrootuser,
+                                printOutput=self.config['print_main_output'])
         results = glob.glob("%s/%s/SRPMS/*src.rpm" % (self.make_chroot_path(),
                                                       self.buildroot.builddir))
         if len(results) != 1:
