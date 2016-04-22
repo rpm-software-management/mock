79259cfa2de1faf798ba67b41fc66dbc043638c1
Miroslav Suchý <msuchy@redhat.com>
2016-04-22 11:57:48 +0200

    W:133, 8: Attribute sources defined outside __init__ (attribute-defined-outside-init)

--- py/mockbuild/scm.py
+++ py/mockbuild/scm.py
@@ -24,6 +24,7 @@ class scmWorker(object):
         self.log.debug("Initializing SCM integration...")
 
         self.name = self.version = None
+        self.sources = []
         self.macros = macros
 
         self.method = opts['method']
