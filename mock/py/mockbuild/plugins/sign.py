32c35802607af8045e1a2051322974f0cf270253
Miroslav Suchý <msuchy@redhat.com>
2018-05-09 12:53:27 +0200

    sign package only if there is something to sign
    addressing:
     # /usr/bin/dnf builddep --installroot /var/lib/mock/fedora-27-x86_64-16920-11732/root/ --releasever 27 -C /var/lib/mock/fedora-27-x86_64-16920-11732/root//builddir/build/SRPMS/test-E-1.1-0.src.rpm --setopt=tsflags=nocontexts
    Last metadata expiration check: 0:02:15 ago on Pá 4. května 2018, 15:40:43 CEST.
    No matching package to install: 'packagewhichdoesnotexist'
    Not all dependencies satisfied
    Error: Some packages could not be found.
    
    During handling of the above exception, another exception occurred:
    
    Traceback (most recent call last):
      File "/usr/libexec/mock/mock", line 965, in <module>
        main()
      File "/usr/lib/python3.6/site-packages/mockbuild/trace_decorator.py", line 96, in trace
        result = func(*args, **kw)
      File "/usr/libexec/mock/mock", line 759, in main
        run_command(options, args, config_opts, commands, buildroot, state)
      File "/usr/lib/python3.6/site-packages/mockbuild/trace_decorator.py", line 96, in trace
        result = func(*args, **kw)
      File "/usr/libexec/mock/mock", line 849, in run_command
        do_rebuild(config_opts, commands, buildroot, args)
      File "/usr/lib/python3.6/site-packages/mockbuild/trace_decorator.py", line 96, in trace
        result = func(*args, **kw)
      File "/usr/libexec/mock/mock", line 536, in do_rebuild
        post=post_build, clean=clean)
      File "/usr/lib/python3.6/site-packages/mockbuild/trace_decorator.py", line 96, in trace
        result = func(*args, **kw)
      File "/usr/libexec/mock/mock", line 481, in rebuild_generic
        ret = cmd(item)
      File "/usr/libexec/mock/mock", line 517, in build
        check=config_opts['check'])
      File "/usr/lib/python3.6/site-packages/mockbuild/trace_decorator.py", line 96, in trace
        result = func(*args, **kw)
      File "/usr/lib/python3.6/site-packages/mockbuild/backend.py", line 304, in build
        self.plugins.call_hooks('postbuild')
      File "/usr/lib/python3.6/site-packages/mockbuild/trace_decorator.py", line 96, in trace
        result = func(*args, **kw)
      File "/usr/lib/python3.6/site-packages/mockbuild/plugin.py", line 78, in call_hooks
        hook(*args, **kwargs)
      File "/usr/lib/python3.6/site-packages/mockbuild/plugins/sign.py", line 35, in sign_results
        rpms = [os.path.join(self.buildroot.resultdir, rpm) for rpm in self.buildroot.final_rpm_list]
    TypeError: 'NoneType' object is not iterable

--- mock/py/mockbuild/plugins/sign.py
+++ mock/py/mockbuild/plugins/sign.py
@@ -32,11 +32,12 @@ class Sign(object):
         getLog().info("enabled package signing")
 
     def sign_results(self):
-        rpms = [os.path.join(self.buildroot.resultdir, rpm) for rpm in self.buildroot.final_rpm_list]
-        if rpms:
-            getLog().info("Signing %s", ', '.join(rpms))
-            opts = self.conf['opts'] % {'rpms': ' '.join(rpms), 'resultdir': self.buildroot.resultdir}
-            cmd = "{0} {1}".format(self.conf['cmd'], opts)
-            getLog().info("Executing %s", cmd)
-            with self.buildroot.uid_manager:
-                subprocess.call(cmd, shell=True, env=os.environ)
+        if self.buildroot.final_rpm_list:
+            rpms = [os.path.join(self.buildroot.resultdir, rpm) for rpm in self.buildroot.final_rpm_list]
+            if rpms:
+                getLog().info("Signing %s", ', '.join(rpms))
+                opts = self.conf['opts'] % {'rpms': ' '.join(rpms), 'resultdir': self.buildroot.resultdir}
+                cmd = "{0} {1}".format(self.conf['cmd'], opts)
+                getLog().info("Executing %s", cmd)
+                with self.buildroot.uid_manager:
+                    subprocess.call(cmd, shell=True, env=os.environ)
