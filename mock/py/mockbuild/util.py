f212a87404a63f271b407ed1ead8561a34dedae4
Toshio Kuratomi <a.badger@gmail.com>
2019-03-14 15:35:00 -0700

    Fix mock for non-ascii paths on python2
    Many paths in mock are a combination of a directory sepcified in
    configuration with a subdirectory read in from the filesystem (or
    specified on the commandline or from some other system service).  The
    configuration paths were made into text strings by the Jinja2 templated
    configs feature.  On python2 (el6 and el7) the strings from the
    filesystem are byte strings.  When the two string types are combined, a
    UnicodeError is thrown.
    
    Rather than find all the places where mock is loading byte strings and
    would need to transform them back into text strings, this patch merely
    changes the text strings returned from Jinja2 into native strings (ie:
    text strings on python3 and byte strings on python2).  This should
    restore the behaviour of the pre-Jinja2 configuration.

--- mock/py/mockbuild/util.py
+++ mock/py/mockbuild/util.py
@@ -159,7 +159,36 @@ class TemplatedDictionary(MutableMapping):
             return value
     def __render_string(self, value):
         template = jinja2.Template(value)
-        return template.render(self.__dict__)
+        return _to_native(template.render(self.__dict__))
+
+
+def _to_bytes(obj, encoding='utf-8', errors='strict', nonstring='strict'):
+    if isinstance(obj, six.binary_type):
+        return obj
+    elif isinstance(obj, six.text_type):
+        return obj.encode(encoding, errors)
+    else:
+        if nonstring == 'strict':
+            raise TypeError('First argument must be a string')
+        raise ValueError('nonstring must be one of: ["strict",]')
+
+
+def _to_text(obj, encoding='utf-8', errors='strict', nonstring='strict'):
+    if isinstance(obj, six.text_type):
+        return obj
+    elif isinstance(obj, six.binary_type):
+        return obj.decode(encoding, errors)
+    else:
+        if nonstring == 'strict':
+            raise TypeError('First argument must be a string')
+        raise ValueError('nonstring must be one of: ["strict",]')
+
+
+if six.PY2:
+    _to_native = _to_bytes
+else:
+    _to_native = _to_text
+
 
 @traceLog()
 def get_proxy_environment(config):
