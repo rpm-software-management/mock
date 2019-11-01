1f7c58fa670562cb53e78520f4b9e75cdc8a50e0
Miroslav Suchý <msuchy@redhat.com>
2019-11-01 10:07:53 +0100

    add traceLog to podman methods

--- mock/py/mockbuild/podman.py
+++ mock/py/mockbuild/podman.py
@@ -2,34 +2,40 @@
 # vim: noai:ts=4:sw=4:expandtab
 
 import subprocess
-from mockbuild.trace_decorator import getLog
+from mockbuild.trace_decorator import getLog, traceLog
 from mockbuild import util
 
 class Podman:
     """ interacts with podman to create build chroot """
+
+    @traceLog()
     def __init__(self, buildroot, image):
         self.buildroot = buildroot
         self.image = image
         self.container_id = None
         getLog().info("Using bootstrap image: %s", image)
 
+    @traceLog()
     def pull_image(self):
         """ pull the latest image """
         getLog().info("Pulling image: %s", self.image)
         cmd = ["podman", "pull", self.image]
         util.do(cmd, printOutput=True)
 
+    @traceLog()
     def get_container_id(self):
         """ start a container and detach immediately """
         cmd = ["podman", "run", "-it", "--detach", self.image, "/bin/bash"]
         container_id = util.do(cmd, returnOutput=True)
         self.container_id = container_id.strip()
 
+    @traceLog()
     def exec(self, command):
         """ make sure the image contains expected packages """
         cmd = ["podman", "exec", self.container_id] + command
         util.do(cmd, printOutput=True)
 
+    @traceLog()
     def export(self, cache_file_name, compress_program):
         """ export container and compress it  """
         getLog().info("Exporting container: %s as %s", self.image, cache_file_name)
@@ -42,6 +48,7 @@ class Podman:
         podman.communicate()
         cache_file.close()
 
+    @traceLog()
     def remove(self):
         """ remove the container """
         cmd = ["podman", "rm", "-f", self.container_id]
