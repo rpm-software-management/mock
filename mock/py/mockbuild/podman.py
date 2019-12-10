d2e9abc5926b361636f4fe92de635b2894247708
Pavel Raiskup <praiskup@redhat.com>
2019-12-10 09:08:22 +0100

    mock: don't use buildroot.pkg_manager in podman
    That attribute is used for 'host -> bootstrap' installation, not for
    'bootstrap -> bootstrap'.
    
    Fixes: #437

--- mock/py/mockbuild/podman.py
+++ mock/py/mockbuild/podman.py
@@ -39,8 +39,12 @@ class Podman:
     @traceLog()
     def install_pkgmgmt_packages(self):
         """ make sure the image contains expected packages """
-        cmd = [self.buildroot.pkg_manager.command, '-y']
-        cmd += self.buildroot.pkg_manager.install_command.split()
+        pmname = self.buildroot.config.get('package_manager', 'dnf')
+        binary = self.buildroot.config.get('{}_command'.format(pmname))
+        install_command = self.buildroot.config.get('{}_install_command'.format(pmname))
+
+        cmd = [binary, '-y']
+        cmd += install_command.split()
         self.exec(cmd)
 
     @traceLog()
