334ef89173d3703be12dae34575b34f36ecf15c2
Pavel Raiskup <praiskup@redhat.com>
2020-03-04 16:17:37 +0100

    opensuse: copy ssl ca bundle to correct chroot path
    OpenSUSE uses different pattern for ssl trusted CAs from Fedora, and to
    actually make dnf (or rather transitively curl) inside bootstrap chroot
    work we need to install the bundle onto
    '/var/lib/ca-certificates/ca-bundle.pem'.
    
    Fixes: #500
    Supersedes: #509

--- mock/py/mockbuild/util.py
+++ mock/py/mockbuild/util.py
@@ -1013,6 +1013,8 @@ def setup_default_config_opts(unprivUid, version, pkgpythondir):
 
     config_opts['redhat_subscription_required'] = False
 
+    config_opts['ssl_ca_bundle_path'] = None
+
     # (global) plugins and plugin configs.
     # ordering constraings: tmpfs must be first.
     #    root_cache next.
