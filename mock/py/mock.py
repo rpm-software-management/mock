b5e8d802c8c77c5bf5b3618fc8f52fa7008d1e73
Pavel Raiskup <praiskup@redhat.com>
2020-03-13 20:19:23 +0100

    mock: use 'private' mount option, not 'rprivate'
    The 'mount --rbind -o rprivate' on EL7 does weird things;  it looks like
    it actually reverses the '--rbind' option and does only '--bind'.  Using
    just 'private' seems to do the same (good) thing on both EL7 and modern
    Fedora.
    
    This fixes local baseurl=file:// repositories with bootstrap, and
    yum_chache plugin on EL7.
    
    Fixes: #381

--- mock/py/mock.py
+++ mock/py/mock.py
@@ -747,15 +747,15 @@ def main():
         # add the extra bind mount to the outer chroot
         inner_mount = bootstrap_buildroot.make_chroot_path(buildroot.make_chroot_path())
 
-        # Hide re-mounted chroot from host by rprivate tmpfs.
+        # Hide re-mounted chroot from host by private tmpfs.
         buildroot.mounts.managed_mounts.append(
             FileSystemMountPoint(filetype='tmpfs',
                                  device='hide_root_in_bootstrap',
                                  path=inner_mount,
-                                 options="rprivate"))
+                                 options="private"))
         buildroot.mounts.managed_mounts.append(
             BindMountPoint(buildroot.make_chroot_path(), inner_mount,
-                           recursive=True, options="rprivate"))
+                           recursive=True, options="private"))
 
     signal.signal(signal.SIGTERM, partial(handle_signals, buildroot))
     signal.signal(signal.SIGPIPE, partial(handle_signals, buildroot))
