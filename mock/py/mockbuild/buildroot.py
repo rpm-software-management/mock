df2b560a172bc39803952f5acda1214a9213a474
Dominik Turecek <dturecek@redhat.com>
2020-09-01 11:51:12 +0200

    Add a new "postupdate" hook
    This hook is used to update the LVM snapshot or root cache tarball after
    the chroot is updated.
    
    Fixes: RHBZ#1175346
    Closes: #594

--- mock/py/mockbuild/buildroot.py
+++ mock/py/mockbuild/buildroot.py
@@ -216,7 +216,12 @@ class Buildroot(object):
                     and self.config['clean']):
                 update_state = "{0} update".format(self.pkg_manager.name)
                 self.state.start(update_state)
+                packages_before = self.all_chroot_packages()
                 self.pkg_manager.update()
+                packages_after = self.all_chroot_packages()
+                if packages_before != packages_after:
+                    self.plugins.call_hooks('postupdate')
+                    self.bootstrap_buildroot.plugins.call_hooks('postupdate')
                 self.state.finish(update_state)
         else:
             self._fixup_build_user()
