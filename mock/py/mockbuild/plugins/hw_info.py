d8e7013759f4ac5887e437d2f31066536240e88f
Miroslav Suchý <msuchy@redhat.com>
2022-02-18 17:16:44 +0100

    add cachedir to output of hw_info plugin
    Resolves: #849

--- mock/py/mockbuild/plugins/hw_info.py
+++ mock/py/mockbuild/plugins/hw_info.py
@@ -47,9 +47,9 @@ class HwInfo(object):
         out.write("\n\nMemory:\n")
         out.write(output)
 
-        cmd = ["/bin/df", "-H", self.buildroot.make_chroot_path()]
+        cmd = ["/bin/df", "-H", self.buildroot.make_chroot_path(), self.config["cache_topdir"]]
         output = mockbuild.util.do(cmd, shell=False, returnOutput=True, raiseExc=False)
-        out.write("\n\nStorage:\n")
+        out.write("\n\nStorage (chroot, cache_topdir):\n")
         out.write(output)
 
         out.close()
