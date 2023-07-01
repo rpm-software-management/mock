9e739d24e3259f79e51ecb371d99c8b724dac0ad
Pavel Raiskup <praiskup@redhat.com>
2023-07-01 21:22:45 +0200

    Kill remaining processes before cleaning-up the chroot
    Should fix bugs like:
    https://bugzilla.redhat.com/2169055
    https://bugzilla.redhat.com/2176689
    https://bugzilla.redhat.com/2176691
    https://bugzilla.redhat.com/2177680
    https://bugzilla.redhat.com/2181641

--- mock/py/mockbuild/backend.py
+++ mock/py/mockbuild/backend.py
@@ -90,6 +90,7 @@ class Commands(object):
         if self.backup:
             self.backup_results()
         self.state.start("clean chroot")
+        util.orphansKill(self.buildroot.make_chroot_path())
         self.buildroot.delete()
         self.state.finish("clean chroot")
 
