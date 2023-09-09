rpm-software-management/mock

commit f7b0c9bf18c46046be922adc740d1c6616ae0cc6

mock: revamp the logging logic
Previously, when bootstrap was enabled, the buildroots were flipping the
target log files all the time from '<root>-bootstrap/results' to
'<root>/results' and back, anytime _resetLogging() was called.  This
also means that the first log entry in the target chroot resultdir
(<root>-bootstrap/results) was created too late, after the bootstrap
chroot was fully initialized - losing a lot of useful information.

Now we never reset the logging for the bootstrap chroot.  This was
always a wrong thing - both buildroot objects share the same
self.root_log/self.build_log logging objects (same object id()).

I renamed Buildroot._resetLogging() to Buildroot.resetLogging(), as it
is actually a public method called from the outside.

Now we call resetLogging(), and thus create the resultdir for logfiles
much earlier — before we even start initializing the bootstrap chroot.
This makes the logs complete.

Closes: #1106
Relates: https://pagure.io/releng/issue/11446
Fixes: #539

mock/py/mockbuild/buildroot.py
@@ -170,7 +170,7 @@ class Buildroot(object):
         return new_path
 
     @traceLog()
-    def initialize(self, prebuild=False, do_log=True):
+    def initialize(self, prebuild=False):
         """
         Initialize the buildroot to a point where it's possible to execute
         commands in chroot. If it was already initialized, just lock the shared
@@ -178,13 +178,11 @@ class Buildroot(object):
         """
         try:
             self._lock_buildroot(exclusive=True)
-            self._init(prebuild=prebuild, do_log=do_log)
+            self._init(prebuild=prebuild)
         except BuildRootLocked:
             self._init_locked()
         finally:
             self._lock_buildroot(exclusive=False)
-        if do_log:
-            self._resetLogging()
 
     @traceLog()
     def chroot_is_initialized(self):
@@ -209,7 +207,7 @@ class Buildroot(object):
         self.set_package_manager()
 
     @traceLog()
-    def _init(self, prebuild, do_log):
+    def _init(self, prebuild):
 
         self.state.start("chroot init")
         file_util.mkdirIfAbsent(self.basedir)
@@ -232,8 +230,7 @@ class Buildroot(object):
             podman.cp(self.make_chroot_path(), self.config["tar_binary"])
 
         self._setup_dirs()
-        if do_log:
-            self._resetLogging()
+
         # /dev is later overwritten by systemd-nspawn, but we need this for
         # initial installation when chroot is empty
         self._setup_devices()
@@ -512,26 +509,21 @@ class Buildroot(object):
                     f.write(l + '\n')
 
     @traceLog()
-    def _resetLogging(self, force=False):
-        # TODOs:
-        # - we should put all bootstrap logs to - say - bootstrap.log
-        # - _resetLogging() actually needs to be called only once - only for
-        #   target chroot (or ideally on global level, through
-        #   'logging.getLogger()' and not through `self.*log` reference)
-        # - we should **not** drop _all_ handlers here when force=True, but only
-        #   those that are intended to be replaced
-        # - 'Mock Version' message should only go to log file, not to stderr
-        # - we need to call this method much, much sooner, ideally before
-        #   bootstrap_chroot.initialize() is called;  otherwise at least
-        #   root.log is far from incomplete
+    def resetLogging(self, force=False):
+        """
+        Pre-create a result directory for log files, initialize the log files
+        there, and identify Mock by dumping there it's version
+        """
+        assert not self.is_bootstrap
 
         # ensure we dont attach the handlers multiple times.
         if self.logging_initialized and not force:
             return
         self.logging_initialized = True
 
+        self._setup_result_dir()
+
         with self.uid_manager:
-            file_util.mkdirIfAbsent(self.resultdir)
             # attach logs to log files.
             # This happens in addition to anything that
             # is set up in the config file... ie. logs go everywhere

mock/py/mockbuild/backend.py
@@ -156,8 +156,11 @@ class Commands(object):
         return self.buildroot.make_chroot_path(*args)
 
     @traceLog()
-    def init(self, **kwargs):
+    def init(self, do_log=True, **kwargs):
         try:
+            if do_log:
+                self.buildroot.resetLogging()
+
             if self.bootstrap_buildroot is not None:
                 file_util.mkdirIfAbsent(self.buildroot.make_chroot_path())
                 self.bootstrap_buildroot.initialize(**kwargs)
@@ -479,7 +482,7 @@ class Commands(object):
                     pdn = s_pkg.replace('.src.rpm', '')
                     resultdir = os.path.join(self.config['local_repo_dir'], pdn)
                     self.buildroot.resultdir = resultdir
-                    self.buildroot._resetLogging(force=True)
+                    self.buildroot.resetLogging(force=True)
                     file_util.mkdirIfAbsent(resultdir)
                     success_file = os.path.join(resultdir, 'success')
                     build_ret_code = 0

commit 71b05584adaf9b61805d35c774474653bfffbfd8

Assure basedir exists before creating resultdir
Because resultdir is, by default, created below basedir.  This has been
breaking use-cases started with commands.init(do_log=True).

The bug was caused by f7b0c9bf18c46046be922adc740d1c6616ae0cc6, where we
started pre-creating resuldir sooner than before so we could start the
logging early.

Fixes: #1183
Closes: #1209
Complements: f7b0c9bf18c46046be922adc740d1c6616ae0cc6

mock/py/mockbuild/buildroot.py
@@ -4,6 +4,7 @@
 from contextlib import contextmanager
 import errno
 import fcntl
+import functools
 import glob
 import grp
 import logging
@@ -37,6 +38,16 @@ def noop_in_bootstrap(f):
             getLog().debug("method {} skipped in bootstrap".format(f.__name__))
             return
         return f(self, *args, **kwargs)
+    return wrapper
+
+
+def call_just_once(f):
+    """ Assure that method returns None, and is called just once """
+    @functools.lru_cache(maxsize=None)
+    @functools.wraps(f)
+    def wrapper(*args, **kwargs):
+        if f(*args, **kwargs) is not None:
+            raise RuntimeError(f"Method '{f.__name__}' can not return values")
     return wrapper
 
 
@@ -194,8 +205,17 @@ class Buildroot(object):
     def chroot_is_initialized(self):
         return os.path.exists(self.make_chroot_path('.initialized'))
 
+    @call_just_once
+    def _setup_basedir(self):
+        file_util.mkdirIfAbsent(self.basedir)
+        mockgid = grp.getgrnam('mock').gr_gid
+        os.chown(self.basedir, os.getuid(), mockgid)
+        os.chmod(self.basedir, 0o775)
+        file_util.mkdirIfAbsent(self.make_chroot_path())
+
     @traceLog()
     def _setup_result_dir(self):
+        self._setup_basedir()
         with self.uid_manager:
             try:
                 file_util.mkdirIfAbsent(self.resultdir)
@@ -254,13 +274,8 @@ class Buildroot(object):
 
     @traceLog()
     def _init(self, prebuild):
-
         self.state.start("chroot init")
-        file_util.mkdirIfAbsent(self.basedir)
-        mockgid = grp.getgrnam('mock').gr_gid
-        os.chown(self.basedir, os.getuid(), mockgid)
-        os.chmod(self.basedir, 0o775)
-        file_util.mkdirIfAbsent(self.make_chroot_path())
+        self._setup_basedir()
         self.plugins.call_hooks('mount_root')
         # intentionally we do not call bootstrap hook here - it does not have sense
         self._setup_nosync()