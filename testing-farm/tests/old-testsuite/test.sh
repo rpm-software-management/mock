731eb21cae8b642507d0fa890abb2d6e88e4f055
Pavel Raiskup <praiskup@redhat.com>
2024-01-26 15:47:24 +0100

    fix

--- testing-farm/tests/old-testsuite/test.sh
+++ testing-farm/tests/old-testsuite/test.sh
@@ -3,7 +3,7 @@
 cd ../../../mock
 install-mock-packages-built-by-packit mock-core-configs mock
 
-if make check 2>&1 | tee the-log | grep FAILED: -e PASSED:; then
+if make check 2>&1 | tee the-log | grep -e FAILED: -e PASSED:; then
     true
 else
     cat the-log
