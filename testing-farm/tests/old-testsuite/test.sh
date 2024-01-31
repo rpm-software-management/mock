e0baa4b95a98612a424fdf873790d6a93d362514
Pavel Raiskup <praiskup@redhat.com>
2024-01-31 06:41:34 +0100

    verbose failure

--- testing-farm/tests/old-testsuite/test.sh
+++ testing-farm/tests/old-testsuite/test.sh
@@ -5,8 +5,10 @@ cd ../../../mock
 install-mock-packages-built-by-packit mock-core-configs mock
 
 if make check >the-log 2>&1; then
+    grep -e FAILED: -e PASSED: the-log
     echo "The 'make check' testsuite passed."
 else
     grep -e FAILED: -e PASSED: the-log
+    cat the-log
     exit 1
 fi
