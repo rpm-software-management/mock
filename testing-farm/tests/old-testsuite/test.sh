e58c7405fdff585d20f49cb3ab02674aec0fe65e
Pavel Raiskup <praiskup@redhat.com>
2024-02-01 11:18:03 +0100

    fix

--- testing-farm/tests/old-testsuite/test.sh
+++ testing-farm/tests/old-testsuite/test.sh
@@ -11,7 +11,10 @@ install-mock-packages-built-by-packit mock-core-configs mock
 # Download the tested SRPM
 SRPM_DOWNLOAD_DIR=/tmp/mock-test-srpms install-mock-packages-built-by-packit mock
 
-if (make check > >(tee the-log | grep -e FAILED: -e PASSED:) 2>&1) >&2; then
+# Testsuite is prepared for the mockbuild user
+
+cd "$workdir/mock"
+if (sudo -E -u mockbuild make check > >(tee the-log | grep -e FAILED: -e PASSED:) 2>&1) >&2; then
     echo "The 'make check' testsuite passed."
 else
     cat the-log
