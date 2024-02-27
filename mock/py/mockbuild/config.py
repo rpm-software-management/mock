0feeeccfa1f619e8141d71a3004e2e3ba8c88315
Jakub Kadlcik <frostyx@email.cz>
2024-02-27 15:56:37 +0100

    Don't use --allowerasing for more DNF commands

--- mock/py/mockbuild/config.py
+++ mock/py/mockbuild/config.py
@@ -328,7 +328,13 @@ def setup_default_config_opts():
     config_opts['dnf5_disable_plugins'] = []
     # No --allowerasing with remove, per
     # https://github.com/rpm-software-management/dnf5/issues/729
-    config_opts["dnf5_avoid_opts"] = {"remove": ["--allowerasing"]}
+    config_opts["dnf5_avoid_opts"] = {
+        "remove": ["--allowerasing"],
+        "repoquery": ["--allowerasing"],
+        "makecache": ["--allowerasing"],
+        "search": ["--allowerasing"],
+        "info": ["--allowerasing"],
+    }
 
     config_opts['microdnf_command'] = '/usr/bin/microdnf'
     # "dnf-install" is special keyword which tells mock to use install but with DNF
