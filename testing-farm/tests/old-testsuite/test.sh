a6003b5cdad2cc1f36006c6de50b12a575bcf0e5
Pavel Raiskup <praiskup@redhat.com>
2025-06-17 17:03:20 +0200

    test

--- testing-farm/tests/old-testsuite/test.sh
+++ testing-farm/tests/old-testsuite/test.sh
@@ -19,11 +19,4 @@ SRPM_DOWNLOAD_DIR=/tmp/mock-test-srpms install-mock-packages-built-by-packit moc
 cd "$workdir/mock"
 
 # shellcheck disable=SC2024
-if (sudo -E -u mockbuild make check > >(tee the-log | grep -e FAILED: -e PASSED:) 2>&1) >&2; then
-    : "The 'make check' testsuite passed, ignoring tests related to specific chroots."
-    cat the-log
-else
-    cat the-log
-    false "The 'make check' testsuite failed."
-    exit 1
-fi
+sudo -E -u mockbuild make check |& tee the-log | grep -e FAILED: -e PASSED:
